a4f9c268344042d818477e94e1c771a37a151e11
MisterKingbad <davymichal20@gmail.com>
2022-10-27 13:15:28 -0300

    feat: :sparkles: Prototype

--- scripts.js
+++ scripts.js
@@ -31,6 +31,8 @@
 
 // console.log(animals[2].name) // Cat
 
+//=================================================================================================
+
 // var weight;
 // console.log(typeof weight) // number typeof é uma função que mostra o tipo da variável
 
@@ -40,40 +42,40 @@
 // let stars = 84.0
 // let isSubscribed = true
 
-let student = {
-    name: 'iri',
-    age: 50,
-    weight:  84.0,
-    isSubscribed: true,
-}
+// let student = {
+//     name: 'iri',
+//     age: 50,
+//     weight:  84.0,
+//     isSubscribed: true,
+// }
 
-console.log(typeof student) // object o typeof mostra o tipo da variável
-console.log(`${student.name} tem ${student.age} anos e pesa ${student.weight}.`)
+// console.log(typeof student) // object o typeof mostra o tipo da variável
+// console.log(`${student.name} tem ${student.age} anos e pesa ${student.weight}.`)
 
-let students = [
+// let students = [
 
-]
+// ]
 
-students = [
-    student
-]
+// students = [
+//     student
+// ]
 
-console.log(students)
+// console.log(students)
 
-console.log(students[0])
+// console.log(students[0])
 
-const john = {
-    name: 'John',
-    age: 23,
-    weight: 83.5,
-    isSubscribed: true
-}
+// const john = {
+//     name: 'John',
+//     age: 23,
+//     weight: 83.5,
+//     isSubscribed: true
+// }
 
-students = [
-    student,
-    john
-]
-console.log(students)
+// students = [
+//     student,
+//     john
+// ]
+// console.log(students)
 
 // // Exercícios
 
@@ -81,3 +83,32 @@ console.log(students)
 /* console.log(a)
 var a = 1 
 resposta: undefined. O hoisting eleva a variável para o topo do código, mas não atribui valor.*/
+
+//========================================================================================================
+
+/*
+    Prototype
+
+    * prototype-based language
+    * prototype chain
+    * __proto__
+    * 
+*/
+
+/*
+    Type conversion (typecasting) vs Type coersion
+
+    * Alteração de um tipo de dado para outro tipo
+
+*/
+
+// console.log('9' + 5) // 95
+// console.log(Number('9') + 5) // 14
+
+// Manipulando Strings e Números
+
+// Transformar String em Número e Número em String
+
+
+
+
