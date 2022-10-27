0465a9c513e3cb4942ab1b1beb4366d027036a70
MisterKingbad <davymichal20@gmail.com>
2022-10-27 10:20:13 -0300

    feat(function): :sparkles: New function named 'function_1'

--- functions/functions.js
+++ functions/functions.js
@@ -1,27 +1,33 @@
-// criar um aplicativo de frases motivacionais
+// podemos declarar funcoes dentro de variaveis e chamado de funcoes anonimas, mas nao e recomendado.
+// function expression
+// function anonymous
 
-// console.log('treinamento e foco é a chave do sucesso')
-// console.log('não desista de treinar')
-// console.log('Paciência e persistência é o caminho do sucesso')
+// parametros da funcao
+// const sum = function(number1, number2) {
+//     console.log(number1 + number2) // com console.log, a funcao nao retorna nada, apenas executa a soma ou seja imprime no console.
+// }
+// sum(25, 25) // arguments - argumentos da funcao
+// sum(18, 18)
+// sum(2, 2.5)	
 
-// declaration - declaração da função
-// function statement
-function createPhrases() {
-    console.log('treinamento e foco é a chave do sucesso')
-    console.log('não desista de treinar')
-    console.log('Paciência e persistência é o caminho do sucesso')
-} //  a funcao esta registrada e guardada na memoria. mas nao foi executada
+// let total = 0 // variavel global
 
-// executar a função
-// rodar, chamar, invocar
-// execute, run, call, invoke
+// const sum = function(number1, number2) {
+//     let total = number1 + number2 // variavel local
+//     return total
+// }
 
-createPhrases() // a funcao foi executada
-createPhrases()
-createPhrases()
-createPhrases()
-createPhrases()
-createPhrases()
+// let number1 = 34
+// let number2 = 25
 
-console.log('fim do programa')
-// pra que serve a funcao? para reutilizar o codigo e agrupamento de codigos. Tambem dar significado ao bloco de codigo.
+// console.log(`o numero 1 é ${number1}`)
+// console.log(`o numero 2 é ${number2}`)
+// console.log(`a soma é ${sum(number1, number2)}`)
+// console.log(total)
+
+// function fazerSuco(fruta1, fruta2) {
+//     return `soco de: ${fruta1+fruta2}` // return finaliza a funcao e retorna o valor da funcao para o local onde foi chamada. 
+// }
+// const copo = fazerSuco('banana', 'whey')
+
+// console.log(copo)
---
+++ functions/functions_1.js
@@ -0,0 +1,27 @@
+// criar um aplicativo de frases motivacionais
+
+// console.log('treinamento e foco é a chave do sucesso')
+// console.log('não desista de treinar')
+// console.log('Paciência e persistência é o caminho do sucesso')
+
+// declaration - declaração da função
+// function statement
+function createPhrases() {
+    console.log('treinamento e foco é a chave do sucesso')
+    console.log('não desista de treinar')
+    console.log('Paciência e persistência é o caminho do sucesso')
+} //  a funcao esta registrada e guardada na memoria. mas nao foi executada
+
+// executar a função
+// rodar, chamar, invocar
+// execute, run, call, invoke
+
+createPhrases() // a funcao foi executada
+createPhrases()
+createPhrases()
+createPhrases()
+createPhrases()
+createPhrases()
+
+console.log('fim do programa')
+// pra que serve a funcao? para reutilizar o codigo e agrupamento de codigos. Tambem dar significado ao bloco de codigo.
