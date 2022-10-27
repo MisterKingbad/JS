bf49f0aa5041406e019a783c2702dcb648e1e5af
MisterKingbad <davymichal20@gmail.com>
2022-10-27 11:38:32 -0300

    feat(function): :sparkles: new functions

--- functions/functions.js
+++ functions/functions.js
@@ -31,3 +31,79 @@
 // const copo = fazerSuco('banana', 'whey')
 
 // console.log(copo)
+
+// //function scope
+// let subject = 'create video'
+// function createThink(subject) {
+//     subject = 'study'
+//     return subject
+// }
+// console.log(createThink(subject))
+// console.log(subject)
+
+// Function hoisting
+
+// sayMyname();
+
+// function sayMyname() {
+//     console.log('irineu')
+// }
+
+// // Arrow function
+
+// const sayMyname = (name) =>{
+//     console.log(name)
+// }
+// sayMyname('Irineu')
+
+// Callback function
+
+// function sayMyname(name) {
+//     console.log('antes de executar a funcao callback')
+//     name() // callback function - funcao que sera executada dentro de outra funcao e so sera executada quando for chamada. 
+//     console.log('depois de executar a funcao callback')
+// }
+
+// sayMyname(
+//     () => { 
+//         console.log('estou em uma callback') // funcao sayMyname recebe uma funcao anonima como parametro e executa a funcao anonima. 
+//     }
+// )
+
+/*
+    Function() constructor
+
+    * expressao new
+    * criar um novo objeto
+    * this keyword
+*/
+
+// function Person(name, age) {
+//     this.name = name // this.name - referencia o atributo name do objeto criado. this - referencia o objeto criado.
+
+//     this.age = age
+
+//     this.walk = function () {
+//         return this.name + " está andando"
+//     }
+
+//     this.noWalk = function () {
+//         return this.name + " não está andando"
+//     }
+    
+//     this.abrirPorta = function () {
+//         return this.name + " abriu a porta"
+//     }
+
+//     this.fecharPorta = function () {
+//         return this.name + " fechou a porta"
+//     }
+// }
+
+// const iri = new Person('Irineu', 25) // new - cria um novo objeto
+// const joao = new Person('Joao')
+
+// console.log(iri)
+
+// console.log(iri.abrirPorta())
+// console.log(joao.fecharPorta())
