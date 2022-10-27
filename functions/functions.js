7621bedef8e1c26d491b9136163f022f34c8539e
MisterKingbad <davymichal20@gmail.com>
2022-10-27 09:34:48 -0300

    feat: :sparkles: function statement

---
+++ functions/functions.js
@@ -0,0 +1,13 @@
+// criar um aplicativo de frases motivacionais
+
+// console.log('treinamento e foco é a chave do sucesso')
+// console.log('não desista de treinar')
+// console.log('Paciência e persistência é o caminho do sucesso')
+
+// declaration - declaração da função
+// function statement
+function createPhrases() {
+    console.log('treinamento e foco é a chave do sucesso')
+    console.log('não desista de treinar')
+    console.log('Paciência e persistência é o caminho do sucesso')
+}
