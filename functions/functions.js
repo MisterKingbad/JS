52dedbcb52da922aaecafd4b9a9d26083b8072c3
MisterKingbad <davymichal20@gmail.com>
2022-10-27 09:45:29 -0300

    feat(function): :sparkles: Running the function

--- functions/functions.js
+++ functions/functions.js
@@ -10,4 +10,18 @@ function createPhrases() {
     console.log('treinamento e foco é a chave do sucesso')
     console.log('não desista de treinar')
     console.log('Paciência e persistência é o caminho do sucesso')
-}
+} //  a funcao esta registrada e guardada na memoria. mas nao foi executada
+
+// executar a função
+// rodar, chamar, invocar
+// execute, run, call, invoke
+
+createPhrases() // a funcao foi executada
+createPhrases()
+createPhrases()
+createPhrases()
+createPhrases()
+createPhrases()
+
+console.log('fim do programa')
+// pra que serve a funcao? para reutilizar o codigo e agrupamento de codigos. Tambem dar significado ao bloco de codigo.
